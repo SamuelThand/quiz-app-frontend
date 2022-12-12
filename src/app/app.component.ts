8f91fc1cfaab8e92f9ef62a008f8f8af306a01b5
Samuel Thand <sath2102@student.miun.se>
2022-12-12 15:58:13 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sath2102_project_frontend_dt190g';
+}
