6bec57bcf30e61f0a8b1ec208ba39e08f567f86a
Samuel Thand <sath2102@student.miun.se>
2022-12-30 15:21:35 +0100

    Built html and css template for admin-home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { ApiDataComponent } from './api-data/api-data.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
@@ -7,7 +8,8 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'api-data', component: ApiDataComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'admin-home', component: AdminHomeComponent }
 ];
 
 @NgModule({
