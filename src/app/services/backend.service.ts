fd1ba258dd7ab380e2c5ec2f431cef5d870afc70
toha2102 <toha2102@student.miun.se>
2023-01-02 12:10:59 +0100

    Added get methods for each model.
    The methods are get-single and get-all and most lack implementation.
    Structured by seperating by model type.

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -28,6 +28,25 @@ export class BackendService {
     this.http = http;
   }
 
+  /* -------------------- Admin methods -------------------- */
+
+  /**
+   * Get an array of all existing Admin objects from the backend.
+   *
+   * @returns Observable with an array of all existing Admin objects
+   */
+  getAdmins(): Observable<Admin[]> {}
+
+  /**
+   * Get the Admin with the specified username.
+   *
+   * @param username username of the Admin
+   * @returns Observable with the Admin object
+   */
+  getAdmin(username: string): Observable<Admin> {}
+
+  /* -------------------- Question methods -------------------- */
+
   /**
    * Get an array of all existing Question objects from the backend.
    *
@@ -40,10 +59,50 @@ export class BackendService {
   /**
    * Get a Question by id.
    *
-   * @param id id of the question
+   * @param id id of the Question
    * @returns Observable with the Question object
    */
   getQuestion(id: number): Observable<Question> {
     return this.http.get<Question>(this.questionEndpoint + id);
   }
+
+  /* -------------------- Quiz methods -------------------- */
+
+  /**
+   * Get an array of all existing Quiz objects from the backend.
+   *
+   * @returns Observable with an array of all existing Quiz objects
+   */
+  getQuizzes(): Observable<Quiz[]> {}
+
+  // TODO: Change this and backend to id (name is not unique, if kept, make name unique)
+  /**
+   * Get a Quiz by name.
+   *
+   * @param name name of the Quiz
+   * @returns Observable with the Quiz object
+   */
+  getQuiz(name: string): Observable<Quiz> {}
+
+  /* -------------------- Subject methods -------------------- */
+
+  /**
+   * Get an array of all existing Subject objects from the backend.
+   *
+   * @returns Observable with an array of all existing Subject objects
+   */
+  getSubjects(): Observable<Subject[]> {}
+
+  /**
+   * Get a Subject by subject code.
+   * The subject code consists of 3 capitalized letters - usually the first three letters of the subject.
+   *
+   * Ex:
+   * Economics -> ECO
+   * Geography -> GEO
+   *
+   * @param name name of the Quiz
+   * @returns Observable with the Quiz object
+   */
+  getSubject(subjectCode: string): Observable<Subject> {}
 }
