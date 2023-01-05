779efaf41fcb9f855e164986ca5ea36f3dd34922
Samuel Thand <sath2102@student.miun.se>
2023-01-05 16:17:15 +0100

    Implemented population of quizzes in admin-home component

--- src/app/admin-home/admin-home.component.ts
+++ src/app/admin-home/admin-home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../services/backend.service';
+import { Quiz } from '../models/quiz.model';
 
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
   styleUrls: ['./admin-home.component.css']
 })
-export class AdminHomeComponent {
+export class AdminHomeComponent implements OnInit {
+  quizzes: Quiz[] = [];
+  private backendService: BackendService;
 
+  ngOnInit(): void {
+    this.backendService.getQuizzes().subscribe((quizzes: Quiz[]) => {
+      this.quizzes = quizzes;
+    });
+  }
+
+  constructor(backendService: BackendService) {
+    this.backendService = backendService;
+  }
 }
