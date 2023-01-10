a4f93ad206c26571dd3db4be5096e9687756deb5
toha2102 <toha2102@student.miun.se>
2023-01-11 00:07:07 +0100

    Enabled admins to log out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { BackendService } from './services/backend.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sath2102_project_frontend_dt190g';
+  private backendService: BackendService;
+
+  constructor(backendService: BackendService) {
+    this.backendService = backendService;
+  }
+
+  /**
+   * Get the current admin status.
+   *
+   * @returns true if the user is logged in as admin, false otherwise
+   */
+  protected isAdmin(): boolean {
+    return this.backendService.isAdmin;
+  }
+
+  /**
+   * Logs out the user.
+   */
+  protected onLogout(): void {
+    this.backendService.signOut().subscribe(() => {
+      this.backendService.isAdmin = false;
+    });
+  }
 }
