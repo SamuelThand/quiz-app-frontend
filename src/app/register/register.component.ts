f573538c68a80df25696e24dddb6176e57a53560
toha2102 <toha2102@student.miun.se>
2023-01-11 00:58:16 +0100

    Form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,8 +1,39 @@
+import { Admin } from '../models/admin.model';
+import { BackendService } from '../services/backend.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  private backendService: BackendService;
+  private router: Router;
+  protected isSuccessful = false;
+  protected isPostFailed = false;
+  protected errorMessage = '';
+  protected form: Admin = {
+    firstName: '',
+    lastName: '',
+    userName: '',
+    password: ''
+  };
+
+  constructor(backendService: BackendService, router: Router) {
+    this.backendService = backendService;
+    this.router = router;
+  }
+
+  protected onSubmit() {
+    this.submit();
+  }
+
+  // TODO: Add errorMessage, isSuccessful, isPostFailed and admin username to message if successful
+  private submit() {
+    this.backendService.signUp(this.form).subscribe((response) => {
+      this.isSuccessful = true;
+    });
+  }
+}
